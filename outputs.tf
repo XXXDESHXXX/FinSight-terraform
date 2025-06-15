b519bac722f4ab4dea528b5eb7c61d56da6f325c
XXXDESHXXX <kirya.shvarts@gmail.com>
2025-06-15 13:02:16 +0300

    Create terraform configuration for deploy gke cluster;

--- outputs.tf
+++ outputs.tf
@@ -5,4 +5,3 @@ output "cluster_name" {
 output "kubeconfig_command" {
   value = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${var.region} --project ${var.project_id}"
 }
-d
