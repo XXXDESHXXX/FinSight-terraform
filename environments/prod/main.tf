f7563aa2dfed585b4600b9dd03a5bbe2b93c1b63
XXXDESHXXX <kirya.shvarts@gmail.com>
2025-06-17 11:02:43 +0300

    Create prod environment

---
+++ environments/prod/main.tf
@@ -0,0 +1,38 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+module "gke_cluster" {
+  source       = "../../modules/gke-cluster"
+  cluster_name = var.cluster_name
+  location     = var.zone
+}
+
+module "gcs_backend" {
+  source      = "../../modules/gcs-backend"
+  bucket_name = "gke-finsight-state"
+  location    = var.region
+  writer_member = "serviceAccount:terraform@${var.project_id}.iam.gserviceaccount.com"
+}
+
+module "terraform_sa" {
+  source       = "../../modules/service-account"
+  project_id   = var.project_id
+  account_id   = var.terraform_sa_account_id
+  display_name = "Terraform Service Account"
+  roles        = [
+    "roles/storage.admin",
+  ]
+}
+
+module "db_node_pool" {
+  source       = "../../modules/gke-node-pool"
+  name         = var.db_pool_name
+  cluster      = module.gke_cluster.cluster_name
+  location     = var.zone
+  node_count   = var.db_node_count
+  machine_type = var.db_machine_type
+  disk_size    = var.db_disk_size
+  disk_type    = var.db_disk_type
+}
---
+++ environments/prod/variables.tf
@@ -0,0 +1,23 @@
+variable "project_id" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "zone" {
+  type = string
+}
+variable "cluster_name" {
+  type = string
+}
+
+variable "terraform_sa_account_id" {
+  type = string
+}
+
+
+variable "db_pool_name"     { type = string }
+variable "db_node_count"    { type = number }
+variable "db_machine_type"  { type = string }
+variable "db_disk_size"     { type = number }
+variable "db_disk_type"     { type = string }
