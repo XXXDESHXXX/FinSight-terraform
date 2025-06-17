30abc799ee369930477d684ccb90fed8e5b3814b
XXXDESHXXX <kirya.shvarts@gmail.com>
2025-06-17 11:00:52 +0300

    Create module for gke cluster with variables outputs; Change terraform.tfvars.example

---
+++ modules/gke-cluster/main.tf
@@ -0,0 +1,11 @@
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.location
+
+  initial_node_count       = 1
+  remove_default_node_pool = true
+
+  networking_mode = "VPC_NATIVE"
+
+  ip_allocation_policy {}
+}
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "kubeconfig_command" {
-  value = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --zone ${var.zone} --project ${var.project_id}"
-}
