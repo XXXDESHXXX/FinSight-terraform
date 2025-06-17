95f7ddaaea3f8d73aa6303db42897a51fd7792d1
XXXDESHXXX <kirya.shvarts@gmail.com>
2025-06-17 11:01:27 +0300

    Create module for gke node pool with variables and outputs

---
+++ modules/gke-node-pool/main.tf
@@ -0,0 +1,21 @@
+resource "google_container_node_pool" "node_pool" {
+  name     = var.name
+  cluster  = var.cluster
+  location = var.location
+
+  node_count = var.node_count
+
+  node_config {
+    machine_type = var.machine_type
+    disk_size_gb = var.disk_size
+    disk_type    = var.disk_type
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = [node_count]
+  }
+}
---
+++ modules/gke-node-pool/variables.tf
@@ -0,0 +1,7 @@
+variable "name"         { type = string }
+variable "cluster"      { type = string }
+variable "location"     { type = string }
+variable "node_count"   { type = number }
+variable "machine_type" { type = string }
+variable "disk_size"    { type = number }
+variable "disk_type"    { type = string }
